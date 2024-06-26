8365be766b63dbeebca285b7812fcf56965a8b9b
Lakshay <59942900+Lakshay-G@users.noreply.github.com>
2024-06-26 13:26:33 -0400

    Sentiment Analysis: Textblob, VADER

--- src/sentiment_analysis.py
+++ src/sentiment_analysis.py
@@ -2,6 +2,28 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import json
+from textblob import TextBlob
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 if __name__ == '__main__':
     print('Hello')
+
+    # # Sample memory text
+    # memory_text = "I had a wonderful time at the park."
+
+    # # Create a TextBlob object
+    # blob = TextBlob(memory_text)
+
+    # # Get sentiment
+    # sentiment = blob.sentiment
+    # print(
+    #     f"Polarity: {sentiment.polarity}, Subjectivity: {sentiment.subjectivity}")
+    # Sample memory text
+    memory_text = "I had a time at the park which helped me enjoy a lot."
+
+    # Initialize VADER sentiment analyzer
+    analyzer = SentimentIntensityAnalyzer()
+
+    # Get sentiment scores
+    sentiment_scores = analyzer.polarity_scores(memory_text)
+    print(sentiment_scores)
